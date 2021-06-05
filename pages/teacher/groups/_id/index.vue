71d664f444cd8daf0627712eacda68de8554cfb4
Bekzhan Bershimbekov <Bekzhan99@gmail.com>
2021-06-05 21:15:40 +0600

    teacher groups done

--- pages/teacher/groups/_id/index.vue
+++ pages/teacher/groups/_id/index.vue
@@ -1,31 +1,48 @@
 <template>
-  <div>
+  <div class="space-y-2">
     <p>Breadcrums</p>
-    <div class="flex justify-between">
-      <h1 class="text-3xl">{{group.name}}</h1>
-      <a @click="openSchedule(course.id, group.id)" class="text-2xl mr-4 cursor-pointer border px-6 py-2">Schedule</a>
+    <div class="flex justify-between items-center">
+      <h1 class="text-2xl font-semibold">{{ group.name }}</h1>
+      <a @click="openSchedule(course.id, group.id)" class="btn-primary-outline"
+        >Schedule</a
+      >
     </div>
-    <div class="flex space-x-2">
-      <button @click="changeCourse(course.id)" class="font-medium px-2" v-for="(course,index) in group.courses" :key="index">{{course.name}}</button>
+    <div class="mx-4 space-y-2">
+      <div class="flex gap-x-2">
+        <a
+          @click.prevent="changeCourse(course.id)"
+          class="font-medium px-4 cursor-pointer border rounded-md transition hover:bg-gray-200"
+          v-for="(course, index) in group.courses"
+          :key="index"
+          >{{ course.name }}</a
+        >
+      </div>
+      <p class="text-gray-500">{{ course.description }}</p>
+      <div class="overflow-x-auto shadow-md">
+        <table class="min-w-full divide-y">
+          <thead class="text-left bg-gray-200">
+            <tr>
+              <th class="px-6 py-3 font-semibold">Full name</th>
+              <th class="px-6 py-3 font-semibold">Attendance</th>
+              <th class="px-6 py-3 font-semibold">Score</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y">
+            <tr v-for="data in tableData" :key="data.id">
+              <td class="px-6 py-3">{{ data.first_name + " " + data.last_name }}</td>
+              <td class="px-6 py-3">{{ data.passed_count }}/{{ data.attendances_count }}</td>
+              <td class="px-6 py-3">
+                {{
+                  data.pivot.score === 0
+                    ? data.pivot.score
+                    : data.pivot.score.toFixed(1)
+                }}%
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <p>{{course.description}}</p>
-    <table>
-      <thead>
-        <tr>
-          <td>Full name</td>
-          <td>Attendance</td>
-          <td>Score</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="data in tableData" :key="data.id">
-          <td>{{data.first_name + ' ' + data.last_name}}</td>
-          <td>{{data.passed_count}}/{{data.attendances_count}}</td>
-          <td>{{data.pivot.score === 0 ? data.pivot.score : data.pivot.score.toFixed(1)}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <!-- <pre>{{tableData}}</pre> -->
   </div>
 </template>
 
@@ -35,33 +52,40 @@ export default {
     return {
       group: [],
       course: {},
-      tableData: ''
-    }
+      tableData: ""
+    };
   },
-  async asyncData({$axios, params}){
-    const res = await $axios.$get(`/teacher/groups/${params.id}`)
+  async asyncData({ $axios, params }) {
+    const res = await $axios.$get(`/teacher/groups/${params.id}`);
     return {
       group: res,
       course: res.courses[0],
-      tableData: await $axios.$get(`/teacher/groups/${params.id}/courses/${res.courses[0].id}/students`)
-    }
+      tableData: await $axios.$get(
+        `/teacher/groups/${params.id}/courses/${res.courses[0].id}/students`
+      )
+    };
   },
   methods: {
     async changeCourse(id) {
-      const res = await this.$nuxt.$axios.$get(`/teacher/groups/${this.$route.params.id}/courses/${id}/students`)
+      const res = await this.$nuxt.$axios.$get(
+        `/teacher/groups/${this.$route.params.id}/courses/${id}/students`
+      );
       this.group.courses.forEach(course => {
-        if(course.id === id) {
-          this.course = course
+        if (course.id === id) {
+          this.course = course;
         }
       });
-      this.tableData = res
+      this.tableData = res;
     },
-    openSchedule(courseId, groupId){
-      this.$router.push({name: 'teacher-schedules', query: {
-        courseId,
-        groupId
-      }})
+    openSchedule(courseId, groupId) {
+      this.$router.push({
+        name: "teacher-schedules",
+        query: {
+          courseId,
+          groupId
+        }
+      });
     }
   }
-}
+};
 </script>
